6b1aa7538542b17867afc5f149fb8c21e3936189
Funda Kayatürk <127946747+FundaKayaturk@users.noreply.github.com>
2024-10-11 13:35:21 +0300

    Add files via upload

---
+++ MVC/BLL/DAL/Students.cs
@@ -0,0 +1,17 @@
+#nullable disable
+namespace BLL.DAL
+{
+    public class Students
+    {
+        public int Id { get; set; }
+        
+        public string Name { get; set; }
+        
+        public string Surname { get; set; }
+        
+        public DateTime BirthDate { get; set; }
+        
+        public decimal Gpa { get; set; }
+
+    }
+}
